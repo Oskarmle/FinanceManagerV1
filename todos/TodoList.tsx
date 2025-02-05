a3e27bfbf71271caf5b073fcc12e985ffb2f7791
Oskar Eriksen <omle.eriksen@gmail.com>
2025-02-05 13:45:50 +0100

    simple todo list

---
+++ todos/TodoList.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import react from "react";
+
+export default function TodoList() {
+  return (
+    <View>
+      <Text>Your todos</Text>
+    </View>
+  );
+}
